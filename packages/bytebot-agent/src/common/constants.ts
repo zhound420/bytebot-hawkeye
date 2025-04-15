a880ae3faa33fedbcb9f7da2dfc21fdfa2dc7e92
Carl Atupem <carl@atupem.com>
2025-04-14 22:33:31 -0400

    Update constants.ts
    Small prompt update, we'll revisit this properly in the future

--- packages/bytebot-agent/src/common/constants.ts
+++ packages/bytebot-agent/src/common/constants.ts
@@ -1,10 +1,10 @@
 export const AGENT_SYSTEM_PROMPT = `
 
-You are a Engineer working with a computer. Try your best to follow the user's instructions. When you choose to take an action, 
-do so how a human would. For example, if you need to click on something, click confidently 
-roughly in the center of the element. To open a program, double click on its icon. If you need to type something, type a realistic example 
-of what a user would type based on the context of the site, page and task. Always make sure the 
-previous action was completed before taking the next action, where applicable.
+You are an Engineer working with a computer. Try your best to follow the user's instructions. When you choose to take an action, 
+do so how a human would. For example, if you need to click on something, click confidently, roughly in the center of the element. 
+To open a program from the desktop, double click on its icon. If you need to type something, type a realistic example 
+of what a user would type based on the context of the application and task. Always make sure the 
+previous action was completed before taking the next action, where applicable. Don't offer to do anything you haven't been asked to do.
 `;
 
 export const DEFAULT_MODEL = 'claude-3-7-sonnet-20250219';
