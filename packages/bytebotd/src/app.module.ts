aba6758f9694b15de9c006a69422b045d249d0a7
Carl Atupem <carl@atupem.com>
2025-06-04 20:55:42 -0700

    Debounce input tracking actions

--- packages/bytebotd/src/app.module.ts
+++ packages/bytebotd/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ComputerUseModule } from './computer-use/computer-use.module';
+import { InputTrackingModule } from './input-tracking/input-tracking.module';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,6 +11,7 @@ import { AppService } from './app.service';
       isGlobal: true, // Explicitly makes it globally available
     }),
     ComputerUseModule,
+    InputTrackingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
