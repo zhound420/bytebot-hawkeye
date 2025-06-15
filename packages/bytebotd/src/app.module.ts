10cef44f264046bbd400d3173249c207152a001b
Carl Atupem <carl@atupem.com>
2025-06-14 22:34:22 -0400

    Update proxy deps

--- packages/bytebotd/src/app.module.ts
+++ packages/bytebotd/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ComputerUseModule } from './computer-use/computer-use.module';
 import { InputTrackingModule } from './input-tracking/input-tracking.module';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -9,6 +10,10 @@ import { AppService } from './app.service';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // Explicitly makes it globally available
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: '/opt/noVNC',
+      serveRoot: '/novnc',
     }),
     ComputerUseModule,
     InputTrackingModule,
--- packages/bytebotd/src/main.ts
+++ packages/bytebotd/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    '/websockify',
+    createProxyMiddleware({
+      target: 'http://localhost:6080',
+      ws: true,
+      changeOrigin: true,
+      pathRewrite: { '^/websockify': '/' },
+    }),
+  );
   await app.listen(9990);
 }
 bootstrap();
