dc3f6572d94af6c62206cbf1a4e59a5714f87ce3
zhound420 <34777398+zhound420@users.noreply.github.com>
2025-09-21 22:01:04 -0700

    Enforce screenshot observation guard for Claude Code agent

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,3 +2,4 @@ export * from "./types/messageContent.types";
 export * from "./utils/messageContent.utils";
 export * from "./utils/computerAction.utils";
 export * from "./types/computerAction.types";
+export * from "./constants/agent.constants";
